29f3c7d1442a07bb17efb098d58f6231e237d49e
figo <figo@figo.com>
2018-01-03 11:24:41 +0200

    Khaled Commit

--- routes/web.php
+++ routes/web.php
@@ -7,64 +7,31 @@
 |
 | Here is where you can register web routes for your application. These
 | routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!| routes are loaded by the RouteServiceProvider within a group which
+| routes are loaded by the RouteServiceProvider within a group which
+| routes are loaded by the RouteServiceProvider within a group which
+| routes are loaded by the RouteServiceProvider within a group which
+| routes are loaded by the RouteServiceProvider within a group which
+| routes are loaded by the RouteServiceProvider within a group which
+| routes are loaded by the RouteServiceProvider within a group which
+| routes are loaded by the RouteServiceProvider within a group which
+| routes are loaded by the RouteServiceProvider within a group which
+| routes are loaded by the RouteServiceProvider within a group which
+| routes are loaded by the RouteServiceProvider within a group which
+| routes are loaded by the RouteServiceProvider within a group which
+| routes are loaded by the RouteServiceProvider within a group which
+| routes are loaded by the RouteServiceProvider within a group which
+| routes are loaded by the RouteServiceProvider within a group which
+| routes are loaded by the RouteServiceProvider within a group which
+| routes are loaded by the RouteServiceProvider within a group which
+| routes are loaded by the RouteServiceProvider within a group which
+| routes are loaded by the RouteServiceProvider within a group which
+| routes are loaded by the RouteServiceProvider within a group which
+| routes are loaded by the RouteServiceProvider within a group which
+| routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 |
 */
 
 Route::get('/', function () {
-    return  ('welcome');
+    return view('welcome');
 });
-
- 
